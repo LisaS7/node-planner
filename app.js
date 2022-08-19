ad6b133ba453410510687701376fe145405550d2
forest <mclisa00@gmail.com>
2022-08-19 16:20:01 +0100

    setting up basic node app and files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './index.html'))
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('resource not found!')
+})
+
+app.listen(PORT, () => {
+    console.log('server running')
+})
