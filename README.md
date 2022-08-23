fa0342b3f4266815689a4b0058a9a30cbf694337
forest <mclisa00@gmail.com>
2022-08-23 14:42:53 +0100

    Toggling class for task list

---
+++ README.md
@@ -0,0 +1,10 @@
+This is my first node app. It displays a calendar which can be populated with tasks for the week.
+
+How It Works
+The app runs using a node.js Express server. The calendar grid is generated using JavaScript.
+
+Configuration
+Weekdays and times can be changed in the script.js file.
+
+Todo:
+- Create config file for days and times.
--- public/script.js
+++ public/script.js
@@ -1,32 +1,39 @@
+import {addElement,toggleClass} from './functions.js';
+
+// Page Elements
+const taskRow = document.getElementById('task-row')
 const calendarTable = document.getElementById('calendar-table');
+const dayHeaderRow = document.getElementById('day-headers');
+
+// Config
 const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 const times = ['8am', '10am', '12pm', '2pm', '4pm'];
+const tasks = ['Study', 'Jog', 'Housework'];
 
-function addElement (elementString, parent, content = null) {
-    let element = document.createElement(elementString);
-    parent.appendChild(element);
-    if (content) {
-        element.innerHTML = content;
-    }
-    return element;
-}
+
+// Create tasks list
+tasks.forEach(task => {
+    let taskCell = addElement('td', parent=taskRow, content=task);
+    taskCell.addEventListener('click', () => {
+        toggleClass(taskCell, 'active-task');
+    })
+})
 
 
 // Create header row and populate with days text.
 // One blank cell is add at the start - this will contain the times.
-const dayHeaderRow = addElement('tr', calendarTable);
-addElement('td', dayHeaderRow);
 weekdays.forEach(day => {
-    addElement('td', parent = dayHeaderRow, content = day);
+    addElement('td', parent=dayHeaderRow, content=day);
 })
 
 // Generate rows for each of the times
 // Create a cell for each day with id
 times.forEach(time => {
-    let timeRow = addElement('tr', calendarTable);
-    addElement('td', parent = timeRow, content = time);
+    let timeRow = addElement('tr', parent=calendarTable);
+    addElement('td', parent=timeRow, content=time);
     weekdays.forEach(day => {
-        
-        let cell = addElement('td', timeRow);
+        let cell = addElement('td', parent=timeRow);
+        cell.id = `${day.slice(0,3)}-${time}`;
+        // add event listener for clicks here??
     })
 })
